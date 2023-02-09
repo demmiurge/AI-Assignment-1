a113894d5708997b3b068dad050185665173ae48
GuillemExe <guillem.arrojo@gmail.com>
2023-02-09 18:03:22 +0100

    Added foldout system

--- Assets/Scripts/Steerings/SteeringContext.cs
+++ Assets/Scripts/Steerings/SteeringContext.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class SteeringContext : MonoBehaviour
 {
-    [Header("Linear Constraints")]
+    [Foldout("Linear constraints", styled=true)]
     public float m_MaxAcceleration = 40f;
     public float m_MaxSpeed = 10f;
     public bool m_ClipVelocity = true;
 
-    [Header("Angular Constraints")]
+    [Foldout("Angular constraints", styled = true)]
     public float m_MaxAngularAcceleration = 360f;
     public float m_MaxAngularSpeed = 90f;
     public bool m_ClipAngularSpeed = true;
 
-    [Header("Arrive Related Parameters")]
+    [Foldout("Arrive related parameters", styled = true)]
     public float m_TimeToDesiredSpeed = 0.1f;
     public float m_CloseEnoughRadius = 1f;
     public float m_SlowDownRadius = 5f;
 
-    [Header("Align Related Parameters")]
+    [Foldout("Align related parameters", styled = true)]
     public float m_TimeToDesiredAngularSpeed = 0.1f;
     public float m_CloseEnoughAngle = 2f;
     public float m_SlowDownAngle = 10f;
 
-    [Header("Pursue & Evade Related Parameters")]
+    [Foldout("Pursue & evade related parameters", styled = true)]
     public float m_MaxPredictionTime = 3f;
     public bool m_ShowFutureTargetGizmos = false;
 
-    [Header("Group & Flocking Related Parameters")]
+    [Foldout("Group & flocking related parameters", styled = true)]
     public string m_IdTag;
     public float m_RepulsionThreshold = 15f;
     public float m_CohesionThreshold = 30f;
@@ -39,17 +39,17 @@ public class SteeringContext : MonoBehaviour
     public float m_AlignmentWeight = 0.2f;
     public bool m_AddWanderIfZero = true;
 
-    [Header("Wander Related Parameters")]
+    [Foldout("Wander related parameters", styled = true)]
     public float m_WanderRate = 30f;
     public float m_WanderRadius = 10f;
     public float m_WanderOffset = 20f;
     public bool m_ShowWanderGizmos = false;
     public float m_WanderTargetOrientation = 0f;
 
-    [Header("Seek weights for steerings")]
+    [Foldout("Seek weight for \"around\" sterings", styled = true)]
     public float m_SeekWeight = 0.2f;
 
-    [Header("Obstacle Avoidance Related Parameters")]
+    [Foldout("Obstacle avoidance related parameters", styled = true)]
     public float m_LookAheadLength = 10f;
     public float m_AvoidDistance = 12f;
     public float m_SecondaryWhiskerAngle = 30f;
@@ -57,17 +57,20 @@ public class SteeringContext : MonoBehaviour
     public float m_PerseveranceTime = 0f;
     public bool m_ShowAvoidanceGizmos = false;
 
+    [HideInInspector]
     public float m_PerseveranceElapsed = 0f;
+    [HideInInspector]
     public bool m_Persevering = false;
+    [HideInInspector]
     public Vector3 m_AvoidanceAcceleration = Vector3.zero;
 
-    [Header("Velocity & Speeds")]
+    [Foldout("Velocity & speeds", styled = true, readOnly = true)]
     public Vector3 m_Velocity = Vector3.zero;
     public float m_Speed;
     public float m_AngularSpeed;
 
     //Group Manager
-    [Header("Group Manager Things")]
+    [Foldout("Group manager for group related stuff", styled = true)]
     public GroupManager m_GroupManager;
 
     void Awake()
@@ -75,6 +78,7 @@ public class SteeringContext : MonoBehaviour
         if(m_GroupManager != null)
             m_GroupManager.m_Members.Add(this.gameObject);
     }
+
     // Start is called before the first frame update
     void Start()
     {
