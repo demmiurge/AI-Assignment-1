2e6a1d61a99a553d847a17193d4b956af87b8720
Alejandro Reñones Farré <renonesalejandro@gmail.com>
2023-02-26 10:47:06 +0100

    Added small audio manager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource _sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip, float volume = 1f)
+    {
+        _sfxSource.PlayOneShot(clip, volume);
+    }
+}
