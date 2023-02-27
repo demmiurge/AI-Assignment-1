76753dd3c5eabbdaecb9d1d249b9b5f433dedc6f
Alejandro Reñones Farré <renonesalejandro@gmail.com>
2023-02-28 00:10:06 +0100

    Added fade methods

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,8 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
 
-    [SerializeField] private AudioSource _sfxSource;
+    [SerializeField] private AudioSource _sfxSource, _musicSource;
+    [SerializeField] private float _timeToFade;
 
     private void Awake()
     {
@@ -21,8 +23,47 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _musicSource.Play();
+    }
+
     public void PlaySound(AudioClip clip, float volume = 1f)
     {
         _sfxSource.PlayOneShot(clip, volume);
+    }
+
+    public void FadeOutMusic()
+    {
+        StartCoroutine(DecreaseVolumeOverTime());
+    }
+
+    public void FadeInMusic()
+    {
+        StartCoroutine(IncreaseVolumeOverTime());
+    }
+
+    private IEnumerator IncreaseVolumeOverTime()
+    {
+        float time = 0f;
+
+        while (time < _timeToFade)
+        {
+            _musicSource.volume = 0f + (time / _timeToFade);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator DecreaseVolumeOverTime()
+    {
+        float time = 0f;
+
+        while (time < _timeToFade)
+        {
+            _musicSource.volume = 1f - (time / _timeToFade);
+            time += Time.deltaTime;
+            yield return null;
+        }
     }
 }
